134940ecce89020f98d0b641933e76599cf772c4
phounphonh.ludovic@gmail.com <phounphonh.ludovic@gmail.com>
2023-07-12 17:54:32 +0200

    start learning srping boot api

---
+++ src/main/java/com/roadtocda/apiemployes/ApiemployesApplication.java
@@ -0,0 +1,12 @@
+package com.roadtocda.apiemployes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiemployesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiemployesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/roadtocda/apiemployes/ApiemployesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadtocda.apiemployes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiemployesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
